d9fe7ef1221120a076db1e312755d396610e8427
likagogishvili <96431544+likagogishvili@users.noreply.github.com>
2022-10-25 20:51:47 +0400

    Update geocode.js

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require("postman-request");
 const geocode = (address, callback) => {
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${address}&limit=5&appid=f8578069342171e6ec32807958bcdc8f`;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${address}&limit=5&appid=`;
   request({ url: url, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
