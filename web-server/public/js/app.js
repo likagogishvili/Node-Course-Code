64eb968e95b9372831e93f82d86a663dedbe23e8
likagogishvili <likagogishvili2@gmail.com>
2022-11-11 16:48:06 +0400

    adding html pages

---
+++ web-server/public/js/app.js
@@ -0,0 +1,1 @@
+console.log("Clinet side JS file is loaded")
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,26 +2,28 @@ const path  = require('path')
 const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+
 app.use(express.static(publicDirectoryPath))
 
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Lika",
-      age: "21",
-    },
-    {
-      name: "John",
-      age: "21",
-    },
-  ]);
-});
+// app.get("/help", (req, res) => {
+//   res.send([
+//     {
+//       name: "Lika",
+//       age: "21",
+//     },
+//     {
+//       name: "John",
+//       age: "21",
+//     },
+//   ]);
+// });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>"
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About</h1>"
   
-  );
-});
+//   );
+// });
 
 app.get("/weather", (req, res) => {
   res.send({
